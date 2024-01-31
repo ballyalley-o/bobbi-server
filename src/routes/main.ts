e67baf2ae9ca2724e03b96ad854074ea3b65169c
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-01 00:59:05 +1300

    Add server route for production environment

---
+++ src/routes/main.ts
@@ -0,0 +1,17 @@
+import path from 'path'
+import express, { Application, Request, Response } from 'express'
+import { GLOBAL, __dirname } from '../config'
+import { PATH, PATH_DIR, RESPONSE } from '../constants'
+
+const ENV = 'production'
+
+const serverRoute = (app: Application) => {
+  if (GLOBAL.ENV === ENV) {
+    app.use(express.static(path.join(__dirname, PATH.BUILD_LOC)))
+    app.get('*', (req: Request, res: Response) => res.sendFile(PATH.BUILD_VIEW))
+  } else {
+    app.use(PATH_DIR.HOME, RESPONSE.server)
+  }
+}
+
+export default serverRoute
