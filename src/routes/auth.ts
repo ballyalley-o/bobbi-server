f1606807bf20e13b62deceffff04993116063eef
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-02 11:24:42 +1300

    Update auth routes to include GET method for sign-in

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express'
 import { authController } from '@controllers'
+import { PathDir } from '@constants'
 
 const router = Router()
 
-router.post('/', authController.authSignIn)
+router.get(PathDir.SIGN_IN_PARAM, authController.authSignIn)
+router.post(PathDir.SIGN_IN_PARAM, authController.authSignIn)
+
+const authRouter = router
+export default authRouter
