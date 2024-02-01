84414be32bb45403b46a77ec1b4c3e6e5a9f05d2
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-02 11:24:37 +1300

    Update PathDir class in path-dir.ts

--- src/constants/path-dir.ts
+++ src/constants/path-dir.ts
@@ -2,17 +2,20 @@ import { __dirname } from '@config'
 import { GLOBAL } from '@config'
 import { conNex } from '@utils'
 
-
 export class PathDir {
   static readonly HOME_PARAM = '/'
   static readonly API_PARAM = '/api'
+  static AUTH_PARAM = '/auth'
+  static SIGN_IN_PARAM = '/sign-in'
 
-  static AUTH = '/auth'
-  static SIGN_IN = '/sign-in'
+  private static connex = conNex
 
+  static HOME = this.connex(GLOBAL.API_URL, PathDir.HOME_PARAM)
+  static API = this.connex(PathDir.API_PARAM, GLOBAL.API_VERSION)
+  static API_ROOT = this.connex(PathDir.API_PARAM, GLOBAL.API_VERSION)
+  static AUTH_ROOT = this.connex(PathDir.API_ROOT, PathDir.AUTH_PARAM)
 
-  static HOME = conNex(GLOBAL.API_URL, PathDir.HOME_PARAM)
-  static API = conNex(PathDir.API_PARAM, GLOBAL.API_VERSION)
-  static BUILD_LOC = 'client/.dist'
-  static BUILD_VIEW = conNex(__dirname, 'client', 'dist', 'index.html')
+  // @production
+  static BUILD_LOC = this.connex(__dirname, 'client', '.dist')
+  static BUILD_VIEW = this.connex(__dirname, 'client', '.dist', 'index.html')
 }
