ccf5c4a4756eeefca6bfffae6962d41ec3e265a8
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-01 23:47:04 +1300

    Update response and route configuration

--- src/constants/response.ts
+++ src/constants/response.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express'
-import { GLOBAL } from '../config'
+import { GLOBAL } from '@config'
 
 const RESPONSE = {
   server: (req: Request, res: Response) => {
     const response = {
       name: GLOBAL.APP_NAME,
       status: 'Running',
-      API: GLOBAL.API_VERSION,
+      appServerName: GLOBAL.APP_SERVER_NAME,
+      version: GLOBAL.API_VERSION,
       url: GLOBAL.API_URL,
       port: GLOBAL.PORT,
       environment: GLOBAL.ENV,
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,16 +1,18 @@
 import path from 'path'
 import express, { Application, Request, Response } from 'express'
-import { GLOBAL, __dirname } from '../config'
-import { PATH, PATH_DIR, RESPONSE } from '../constants'
+import { GLOBAL, __dirname } from '@config'
+import { RESPONSE, PathDir } from '@constants'
 
 const ENV = 'production'
 
 const serverRoute = (app: Application) => {
   if (GLOBAL.ENV === ENV) {
-    app.use(express.static(path.join(__dirname, PATH.BUILD_LOC)))
-    app.get('*', (req: Request, res: Response) => res.sendFile(PATH.BUILD_VIEW))
+    app.use(express.static(path.join(__dirname, PathDir.BUILD_LOC)))
+    app.get('*', (req: Request, res: Response) =>
+      res.sendFile(PathDir.BUILD_VIEW)
+    )
   } else {
-    app.get(PATH_DIR.API, RESPONSE.server)
+    app.get(PathDir.API, RESPONSE.server)
   }
 }
 
