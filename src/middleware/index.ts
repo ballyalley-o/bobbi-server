992b86c010ce15bfeea764cd0905f2e582377ba2
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-02 08:08:59 +1300

    Add new middleware and update logger import

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,1 +1,4 @@
-export { default as logger } from './logger'
+export { default as logger } from '@middleware/logger'
+export { default as notFound } from '@middleware/not-found'
+export { default as errorHandler } from '@middleware/error-handler'
+export { default as asyncHandler } from '@middleware/async-handler'
