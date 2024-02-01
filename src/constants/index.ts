1582ed2e7ad09817bde20f470cfaa6285822cbc9
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-01 23:46:34 +1300

    Update imports in constants/index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,2 @@
-export { default as PATH } from './path'
-export { default as PATH_DIR } from './path-dir'
-export { default as RESPONSE } from './response'
+export { PathDir } from '@constants/path-dir'
+export { default as RESPONSE } from '@constants/response'
