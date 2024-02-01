b1d4c58bc49c717f39d707090a0b5f2f4ef18c21
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-02 11:24:46 +1300

    Add authRouter to linkNex function

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,9 @@
 import { Application } from 'express'
+import authRouter from '@routes/auth'
+import { PathDir } from '@constants'
 
-const linkNex = (app: Application) => {}
+const linkNex = (app: Application) => {
+  app.use(PathDir.AUTH_ROOT, authRouter)
+}
+
+export default linkNex
