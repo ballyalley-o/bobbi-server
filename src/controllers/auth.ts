5d8f28caa1ea8d82c61f16f9b28e8bc038ce98b1
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-03 15:57:22 +1300

    🦄 refactor(Refactor auth controller to include new sign-in functionality):

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,24 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { asyncHandler } from '@middleware'
-import { ExpressCallback } from '@typings'
-import { ELEMENT } from '@constants'
+import { IRequestExtended } from '@interfaces/middleware'
+import { Element } from '@constants'
 
-const authSignIn = asyncHandler(async (req: Request, res: Response) => {
-  res.status(200).send(ELEMENT.LOGIN_FORM)
+const getForm = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).send(Element.SIGNIN_FORM)
 })
 
-const authController = { authSignIn }
+const signIn = asyncHandler(
+  async (req: IRequestExtended, res: Response, next: NextFunction) => {
+    const { email, password } = req.body
+
+    if (email) {
+      req.session = { signedIn: true }
+      res.status(201).send({ message: 'successful attempt', email, password })
+    } else {
+      res.status(400).send({ message: 'failed attempt' })
+    }
+  }
+)
+
+const authController = { getForm, signIn }
 export default authController
