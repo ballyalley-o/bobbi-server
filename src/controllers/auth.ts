e1782f62e599e8490d33bb25cbcebc4567bb1b02
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-04 06:10:00 +1300

    Add sign-out and get-auth routes to auth controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,24 +1,71 @@
 import { NextFunction, Request, Response } from 'express'
 import { asyncHandler } from '@middleware'
 import { IRequestExtended } from '@interfaces/middleware'
-import { Element } from '@constants'
+import { Element, PathDir } from '@constants'
 
+// @desc    - Get the Sign-In Form
+// @route   - GET /auth/sign-in
+// @access  - Public
 const getForm = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).send(Element.SIGNIN_FORM)
 })
 
+// @desc    - Sign-In User
+// @route   - POST /auth/sign-in
+// @access  - Public
 const signIn = asyncHandler(
-  async (req: IRequestExtended, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body
 
-    if (email) {
-      req.session = { signedIn: true }
-      res.status(201).send({ message: 'successful attempt', email, password })
+    if (email === 'user@bobbi.com' && password === '123456') {
+      const user = {
+        id: 1,
+        username: 'userbobbi1',
+        email,
+      }
+
+      req.session = {
+        loggedIn: true,
+        userId: user.id,
+        username: user.username,
+        email: email,
+      }
+
+      res.status(200).redirect(PathDir.AUTH_ROOT)
+      // res.status(200).send(Element.CORE('You are Logged In!'))
     } else {
       res.status(400).send({ message: 'failed attempt' })
     }
   }
 )
 
-const authController = { getForm, signIn }
+// @desc    - Sign-Out User
+// @route   - POST /auth/sign-out
+// @access  - Public
+const signOut = asyncHandler(
+  async (req: IRequestExtended, res: Response, next: NextFunction) => {
+    if (req.session) {
+      req.session = null
+      res.status(200).send(Element.SIGNOUT_REDIR)
+    } else {
+      res.status(401).send({ message: 'User failed to sign-out' })
+    }
+  }
+)
+
+// @desc    - Signed-in  User
+// @route   - GET /auth/
+// @access  - Public
+const getAuth = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.session && req.session.loggedIn) {
+      // req.session = {}
+      res.status(200).send(Element.CORE('You are Logged In'))
+    } else {
+      res.status(401).send(Element.SIGNIN_REDIR)
+    }
+  }
+)
+
+const authController = { getForm, signIn, signOut, getAuth }
 export default authController
