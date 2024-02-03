c38b0dc9379f1126597ed3a81b8bc3d8d9fd3243
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-04 06:10:09 +1300

    Add routes for home and sign out

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,6 +6,9 @@ const router = Router()
 
 router.get(PathDir.SIGN_IN_PARAM, authController.getForm)
 router.post(PathDir.SIGN_IN_PARAM, authController.signIn)
+router.get(PathDir.HOME_PARAM, authController.getAuth)
+router.get(PathDir.SIGN_OUT_PARAM, authController.signOut)
+router.post(PathDir.SIGN_OUT_PARAM, authController.signOut)
 
 const authRouter = router
 export default authRouter
