d1d6f309046c8a78ef8a4cdd84810a20b9309476
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-04 06:26:45 +1300

    Add IExpressController interface to handle extended requests

--- src/interfaces/middleware.ts
+++ src/interfaces/middleware.ts
@@ -1,5 +1,9 @@
-import { Request } from 'express'
+import { Request, Response, NextFunction } from 'express'
 
 export interface IRequestExtended extends Request {
   body: { [key: string]: string | undefined }
+}
+
+export interface IExpressController {
+  (req: IRequestExtended, res: Response, next: NextFunction): void
 }
