81a295ffec1c822a7e5faef119a3137bb6a75996
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-03 15:47:50 +1300

    🎈 perf(Add new path parameter and endpoint in PathDir class):

--- src/constants/path-dir.ts
+++ src/constants/path-dir.ts
@@ -2,17 +2,28 @@ import { __dirname } from '@config'
 import { GLOBAL } from '@config'
 import { conNex } from '@utils'
 
+/**
+ * @class PathDir
+ * @description This class represents the path directory
+ * @returns void
+ */
 export class PathDir {
+  // path parameters
   static readonly HOME_PARAM = '/'
   static readonly API_PARAM = '/api'
+  static readonly HOME_PARAM_v2 = '/home'
   static AUTH_PARAM = '/auth'
   static SIGN_IN_PARAM = '/sign-in'
 
+  /**
+   * Connect the path
+   * @returns void
+   */
   private static connex = conNex
 
-  static HOME = this.connex(GLOBAL.API_URL, PathDir.HOME_PARAM)
-  static API = this.connex(PathDir.API_PARAM, GLOBAL.API_VERSION)
+  // endpoints
   static API_ROOT = this.connex(PathDir.API_PARAM, GLOBAL.API_VERSION)
+  static HOME = this.connex(PathDir.API_ROOT, this.HOME_PARAM_v2)
   static AUTH_ROOT = this.connex(PathDir.API_ROOT, PathDir.AUTH_PARAM)
 
   // @production
