795029d5f77c1a0677ca44e42827f71380478f4c
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-06 12:49:03 +1300

    🦄 refactor(Refactor authentication controller and add sign-in and sign-out functionality):

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,3 @@
 // export { default as authController } from '@controllers/auth'
-export { default as coreController } from '@controllers/core'
+// export { default as coreController } from '@controllers/core'
 // export { router } from '@controllers/auth'
