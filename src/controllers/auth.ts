d69b16634bdb44cee38f0eb9fa1adad5f4c536c4
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-05 13:21:29 +1300

    🦄 refactor(Refactor auth controller and add router (CLASS)):

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,70 +1,142 @@
-import { NextFunction, Request, Response } from 'express'
+import express, { Router, Request, Response } from 'express'
 import { asyncHandler } from '@middleware'
 import { IRequestExtended } from '@interfaces/middleware'
 import { Element, PathDir } from '@constants'
+import { get, controller } from '@decorators'
 
-// @desc    - Get the Sign-In Form
-// @route   - GET /auth/sign-in
-// @access  - Public
-const getForm = asyncHandler(async (req: Request, res: Response) => {
-  res.status(200).send(Element.SIGNIN_FORM)
-})
+// // @desc    - Get the Sign-In Form
+// // @route   - GET /auth/sign-in
+// // @access  - Public
+// const getForm = asyncHandler(async (req: Request, res: Response) => {
+//   res.status(200).send(Element.SIGNIN_FORM)
+// })
 
-// @desc    - Sign-In User
-// @route   - POST /auth/sign-in
-// @access  - Public
-const signIn = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password } = req.body
+// // @desc    - Sign-In User
+// // @route   - POST /auth/sign-in
+// // @access  - Public
+// const signIn = asyncHandler(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const { email, password } = req.body
 
-    if (email === 'user@bobbi.com' && password === '123456') {
-      const user = {
-        id: 1,
-        username: 'userbobbi1',
-        email,
-      }
+//     if (email === 'user@bobbi.com' && password === '123456') {
+//       const user = {
+//         id: 1,
+//         username: 'userbobbi1',
+//         email,
+//       }
 
-      req.session = {
-        signedIn: true,
-        userId: user.id,
-        username: user.username,
-        email: email,
-      }
+//       req.session = {
+//         signedIn: true,
+//         userId: user.id,
+//         username: user.username,
+//         email: email,
+//       }
 
-      res.status(200).redirect(PathDir.AUTH_ROOT)
-    } else {
-      res.status(400).send({ message: 'failed attempt' })
-    }
+//       res.status(200).redirect(PathDir.AUTH_ROOT)
+//     } else {
+//       res.status(400).send({ message: 'failed attempt' })
+//     }
+//   }
+// )
+
+// // @desc    - Sign-Out User
+// // @route   - POST /auth/sign-out
+// // @access  - Public
+// const signOut = asyncHandler(
+//   async (req: IRequestExtended, res: Response, next: NextFunction) => {
+//     if (req.session) {
+//       req.session = null
+//       res.status(200).send(Element.SIGNOUT_REDIR)
+//     } else {
+//       res.status(401).send({ message: 'User failed to sign-out' })
+//     }
+//   }
+// )
+
+// // @desc    - Signed-in  User
+// // @route   - GET /auth
+// // @access  - Public
+// const getAuth = asyncHandler(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     if (req.session && req.session.signedIn) {
+//       // req.session = {}
+//       res.status(200).send(Element.CORE('You are Logged In'))
+//     } else {
+//       res.status(401).send(Element.SIGNIN_REDIR)
+//     }
+//   }
+// )
+
+// const authController = { getForm, signIn, signOut, getAuth }
+// export default authController
+
+// class  refactor
+
+// TODO: root path decorator
+@controller('/auth')
+export class AuthController {
+  // @desc    - Get the Sign-In Form
+  // @route   - GET /auth/sign-in
+  // @access  - Public
+
+  @get('/sign-in')
+  getForm(req: Request, res: Response): void {
+    res.status(200).send(Element.SIGNIN_FORM)
   }
-)
+}
+  // // @desc    - Sign-In User
+  // // @route   - POST /auth/sign-in
+  // // @access  - Public
+  // public signIn = asyncHandler(
+  //   async (req: Request, res: Response, next: NextFunction) => {
+  //     const { email, password } = req.body
 
-// @desc    - Sign-Out User
-// @route   - POST /auth/sign-out
-// @access  - Public
-const signOut = asyncHandler(
-  async (req: IRequestExtended, res: Response, next: NextFunction) => {
-    if (req.session) {
-      req.session = null
-      res.status(200).send(Element.SIGNOUT_REDIR)
-    } else {
-      res.status(401).send({ message: 'User failed to sign-out' })
-    }
-  }
-)
+  //     if (email === 'user@bobbi.com' && password === '123456') {
+  //       const user = {
+  //         id: 1,
+  //         username: 'userbobbi1',
+  //         email,
+  //       }
 
-// @desc    - Signed-in  User
-// @route   - GET /auth
-// @access  - Public
-const getAuth = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    if (req.session && req.session.signedIn) {
-      // req.session = {}
-      res.status(200).send(Element.CORE('You are Logged In'))
-    } else {
-      res.status(401).send(Element.SIGNIN_REDIR)
-    }
-  }
-)
+  //       req.session = {
+  //         signedIn: true,
+  //         userId: user.id,
+  //         username: user.username,
+  //         email: email,
+  //       }
 
-const authController = { getForm, signIn, signOut, getAuth }
-export default authController
+  //       res.status(200).redirect(PathDir.AUTH_ROOT)
+  //     } else {
+  //       res.status(400).send({ message: 'failed attempt' })
+  //     }
+  //   }
+  // )
+
+  // // @desc    - Sign-Out User
+  // // @route   - POST /auth/sign-out
+  // // @access  - Public
+  // public signOut = asyncHandler(
+  //   async (req: IRequestExtended, res: Response, next: NextFunction) => {
+  //     if (req.session) {
+  //       req.session = null
+  //       res.status(200).send(Element.SIGNOUT_REDIR)
+  //     } else {
+  //       res.status(401).send({ message: 'User failed to sign-out' })
+  //     }
+  //   }
+  // )
+
+  // // @desc    - Signed-in  User
+  // // @route   - GET /auth
+  // // @access  - Public
+  // public getAuth = asyncHandler(
+  //   async (req: Request, res: Response, next: NextFunction) => {
+  //     if (req.session && req.session.signedIn) {
+  //       // req.session = {}
+  //       res.status(200).send(Element.CORE('You are Logged In'))
+  //     } else {
+  //       res.status(401).send(Element.SIGNIN_REDIR)
+  //     }
+  //   }
+  // )
+// }
