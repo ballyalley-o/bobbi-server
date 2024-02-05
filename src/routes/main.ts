5bed1a07f4f059ddef928f9918451998bc68aa95
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-05 21:44:14 +1300

    🦄 refactor(Refactor serverRoute function and remove unused code):

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,18 +1,29 @@
 import express, { Application, Request, Response } from 'express'
 import { GLOBAL, __dirname } from '@config'
 import { RESPONSE, PathDir } from '@constants'
+import AppRouter from '@app-router'
 
 const ENV = 'production'
+const router = AppRouter.instance
 
-const serverRoute = (app: Application) => {
+/**
+ * @description
+ *    This function is responsible for routing the server.
+ *
+ * @param app - express application
+ *
+ * @returns void
+ */
+const serverRoute = () => {
   if (GLOBAL.ENV === ENV) {
-    app.use(express.static(PathDir.BUILD_LOC))
-    app.get('*', (req: Request, res: Response) =>
+    router.use(express.static(PathDir.BUILD_LOC))
+    router.get('*', (req: Request, res: Response) =>
       res.sendFile(PathDir.BUILD_VIEW)
     )
   } else {
-    app.get(PathDir.API_ROOT, RESPONSE.server)
+    router.get(PathDir.API_ROOT, RESPONSE.server)
   }
 }
+
 
 export default serverRoute
