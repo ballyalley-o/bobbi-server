8b085b4cade0f93e3eb13b0def16816a93a9ed83
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-05 13:22:07 +1300

    Commented out unnecessary code and imports

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,14 @@
-import { Router } from 'express'
-import { authController } from '@controllers'
-import { PathDir } from '@constants'
+// import { Router } from 'express'
+// // import { authController } from '@controllers'
+// import { PathDir } from '@constants'
 
-const router = Router()
+// const router = Router()
 
-router.get(PathDir.SIGN_IN_PARAM, authController.getForm)
-router.post(PathDir.SIGN_IN_PARAM, authController.signIn)
-router.get(PathDir.HOME_PARAM, authController.getAuth)
-router.get(PathDir.SIGN_OUT_PARAM, authController.signOut)
-router.post(PathDir.SIGN_OUT_PARAM, authController.signOut)
+// router.get(PathDir.SIGN_IN_PARAM, authController.getForm)
+// router.post(PathDir.SIGN_IN_PARAM, authController.signIn)
+// router.get(PathDir.HOME_PARAM, authController.getAuth)
+// router.get(PathDir.SIGN_OUT_PARAM, authController.signOut)
+// router.post(PathDir.SIGN_OUT_PARAM, authController.signOut)
 
-const authRouter = router
-export default authRouter
+// const authRouter = router
+// export default authRouter
