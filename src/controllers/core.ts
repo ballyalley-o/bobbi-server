24ae95155e5c0146c94bdbc488a247478a3ed192
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-06 12:51:12 +1300

    (iSSUE still not fixed)

--- src/controllers/core.ts
+++ src/controllers/core.ts
@@ -1,15 +1,31 @@
 import { Response } from 'express'
-import { asyncHandler } from '@middleware'
 import { IRequestExtended } from '@interfaces/middleware'
 import { Element } from '@constants'
+import { controller, get, use } from '@decorators'
+import { LogRequest, authProtect } from '@decorators'
+import { PathDir } from '@constants'
 
-const getCore = asyncHandler(async (req: IRequestExtended, res: Response) => {
-  if (req.session && req.session.signedIn) {
-    res.status(200).send(Element.CORE('HOME PAGE'))
-  } else {
-    res.status(200).send(Element.SIGNIN_REDIR)
+// const getCore = asyncHandler(async (req: IRequestExtended, res: Response) => {
+//   if (req.session && req.session.signedIn) {
+//     res.status(200).send(Element.CORE('HOME PAGE'))
+//   } else {
+//     res.status(200).send(Element.SIGNIN_REDIR)
+//   }
+// })
+
+// const coreController = { getCore }
+// export default coreController
+
+@controller(PathDir.HOME_PARAM)
+export class HomeController {
+  /**
+   *  Get the Home Page
+   * @param req - IRequestExtended
+   * @param res - Response
+   */
+  @get(PathDir.ORIGIN_PARAM)
+  @use(authProtect)
+  getForm(req: Request, res: Response): void {
+    res.status(200).send(Element.SIGNIN_FORM)
   }
-})
-
-const coreController = { getCore }
-export default coreController
+}
