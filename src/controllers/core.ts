bde51e5d9ad7d946c7d151a992cc955158a388a1
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-06 13:27:59 +1300

    Add debug decorator and update getForm method

--- src/controllers/core.ts
+++ src/controllers/core.ts
@@ -2,7 +2,7 @@ import { Response } from 'express'
 import { IRequestExtended } from '@interfaces/middleware'
 import { Element } from '@constants'
 import { controller, get, use } from '@decorators'
-import { LogRequest, authProtect } from '@decorators'
+import { LogRequest, authProtect, debug } from '@decorators'
 import { PathDir } from '@constants'
 
 // const getCore = asyncHandler(async (req: IRequestExtended, res: Response) => {
@@ -25,7 +25,11 @@ export class HomeController {
    */
   @get(PathDir.ORIGIN_PARAM)
   @use(authProtect)
-  getForm(req: Request, res: Response): void {
-    res.status(200).send(Element.SIGNIN_FORM)
+  getForm(req: IRequestExtended, res: Response): void {
+    if (req.session && req.session.signedIn) {
+      res.status(200).send(Element.CORE('HOME PAGE'))
+    } else {
+      res.status(200).send(Element.SIGNIN_REDIR)
+    }
   }
 }
