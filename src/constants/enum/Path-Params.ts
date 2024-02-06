6c7f9bb5c85d75a9b0136999cbc0d2a958ad33a1
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-06 14:41:31 +1300

    Add PathParam enum and update PathDir to use enum

---
+++ src/constants/enum/Path-Params.ts
@@ -0,0 +1,8 @@
+export enum PathParam {
+  ORIGIN_PARAM = '/',
+  HOME_PARAM = '/home',
+  API_PARAM = '/api',
+  AUTH_PARAM = '/auth',
+  SIGN_IN_PARAM = '/sign-in',
+  SIGN_OUT_PARAM = '/sign-out',
+}
